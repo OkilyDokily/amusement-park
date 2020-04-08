468bca5f00e5fc955f0300081eca9c1ee905a5a6
me <me@me.me.com>
2020-04-08 15:39:13 -0700

    create a kid height array

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,7 @@ $(document).ready(function(){
   $("#formOne").submit(function(e){
     e.preventDefault();
     var scaryHeightArray = [60,58,56];
+    var kidHeightArray = [[36,54]];
     var height = parseInt($("#height").val());
     hide();
     scaryHeightArray.forEach(function(number){
