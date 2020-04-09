5c9d3d922c8afcb7a1a7edb032e9824221983d4e
me <me@me.me.com>
2020-04-09 05:31:00 -0700

    copy event handler for second button

--- js/scripts.js
+++ js/scripts.js
@@ -28,4 +28,33 @@ $(document).ready(function(){
 
 
   });
+  $("#showall").click(function(e){
+    e.preventDefault();
+    var scaryHeightArray = [60,58,56];
+    var kidHeightArray = [[36,54]];
+    var height = parseInt($("#height").val());
+    hide();
+    scaryHeightArray.forEach(function(number){
+      if (height >= number){
+        $(".scary-"+ number).show();
+      }
+    })
+
+    kidHeightArray.forEach(function(item){
+      if (height >= item[0] && height <= item[1]){
+        $(".kid-" + item[0] + "-" + item[1]).show();
+      }
+    });
+    
+    function hide(){
+      scaryHeightArray.forEach(function(number){
+          $(".scary-"+ number).hide();
+      });
+      kidHeightArray.forEach(function(number){
+        $( ".kid-"+ number[0] + "-" + number[1]).hide();
+    });
+    }
+
+
+  });
 });
